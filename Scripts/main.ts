36a5e2a93893c6e1a3b3040e1cdcca6b5c1f5cb7
SabineWren <sabinewrensc@gmail.com>
2021-03-27 01:32:04 -0700

    Added more directory nesting to categories elements

--- Scripts/main.ts
+++ Scripts/main.ts
@@ -1,5 +1,5 @@
-import { MyComponent } from "c-one.js"
-import { MyComponent2 } from "c-two.js"
+import { MyComponent } from "Lit/c-one.js"
+import { MyComponent2 } from "Native/c-two.js"
 
 customElements.define("c-one", MyComponent)
 customElements.define("c-two", MyComponent2)
